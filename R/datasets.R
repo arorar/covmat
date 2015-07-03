e435a304a17524b5151f6939106396f9997a3852
rpackage <emailrohitarora@gmail.com>
2015-07-02 19:12:47 -0700

    Documentation for datasets

---
+++ R/datasets.R
@@ -0,0 +1,19 @@
+#' @name factor.data
+#' @title Factor Data
+#' @description Contains 8 factors. 5 Fama-French factors. 1 Momentum factor. 
+#'              1 Liquidity factor, 1 Volatility factor.
+#' @docType data
+#' @source Ken.French Website, Lubos Pastor website, Yahoo finance
+#' @usage factor.data
+NULL
+
+#' @name symdata
+#' @title Symbol Data
+#' @description Contains returns for 7 symbols ie BABA, TWTR, LNKD, YHOO, GE, 
+#'              LAZ, V starting from 2007-04-01 to 2015-07-02. Data maybe updated
+#'              using the getSymbolData script in the inst folder
+#' @docType data
+#' @source Yahoo finance
+#' @usage symdata
+NULL
+
