fa72a0197f83e24ab729970738fe9acccd2a2aa2
rpackage <emailrohitarora@gmail.com>
2015-08-21 14:15:42 -0700

    small change for test of covUtils

--- tests/testthat/test-covUtils.R
+++ tests/testthat/test-covUtils.R
@@ -4,7 +4,7 @@ symdata <- coredata(etfdata[,1:5])
 
 # compare covarianve matrices
 cov1 <- cov(symdata)
-cov2 <- covRob(symdata)$cov
+cov2 <- cov(symdata)
 
 # if plot is produced then ellipse plot will return the orgiinal list
 y <- compareCov(cov1 = cov1, cov2 = cov2, labels = c("classical", "robust"))
