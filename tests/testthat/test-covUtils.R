985d967007c0abb99bbe6f79bd4e3e229721edd6
rpackage <emailrohitarora@gmail.com>
2015-08-21 13:36:22 -0700

    Changing location for pdf

---
+++ tests/testthat/test-covUtils.R
@@ -0,0 +1,11 @@
+data("etfdata")
+
+symdata <- coredata(etfdata[,1:5])
+
+# compare covarianve matrices
+cov1 <- cov(symdata)
+cov2 <- covRob(symdata)$cov
+
+# if plot is produced then ellipse plot will return the orgiinal list
+y <- compareCov(cov1 = cov1, cov2 = cov2, labels = c("classical", "robust"))
+expect_equal(length(y), 2)
